f17833eefabdabd40935d6b35ba0d0fa79d88966
g4zeru <haru25haru02@gmail.com>
2020-11-12 23:49:22 +0900

    Initial Commit

---
+++ SheepCoffee/SheepCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheepCoffeeApp.swift
+//  SheepCoffee
+//
+//  Created by Haruta Yamada on 2020/11/12.
+//
+
+import SwiftUI
+
+@main
+struct SheepCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
